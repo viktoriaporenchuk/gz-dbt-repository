2b1618be58c556f4ad22c44fddd18e1e41064990
porenchukv@gmail.com <porenchukv@gmail.com>
2025-07-30 15:05:55 +0000

    cc

---
+++ models/intermediate/int_campaigns.sql
@@ -0,0 +1,51 @@
+{{ config(
+    materialized='table'
+) }}
+
+WITH adwords AS (
+    SELECT
+        date,
+        campaign_id,
+        campaign_name,
+        CAST(ads_cost AS FLOAT64) AS ads_cost,
+        'adwords' AS source
+    FROM {{ ref('stg_raw__adwords') }}
+),
+
+bing AS (
+    SELECT
+        date,
+        campaign_id,
+        campaign_name,
+        CAST(ads_cost AS FLOAT64) AS ads_cost,
+        'bing' AS source
+    FROM {{ ref('stg_raw__bing') }}
+),
+
+criteo AS (
+    SELECT
+        date,
+        campaign_id,
+        campaign_name,
+        CAST(ads_cost AS FLOAT64) AS ads_cost,
+        'criteo' AS source
+    FROM {{ ref('stg_raw__criteo') }}
+),
+
+facebook AS (
+    SELECT
+        date,
+        campaign_id,
+        campaign_name,
+        CAST(ads_cost AS FLOAT64) AS ads_cost,
+        'facebook' AS source
+    FROM {{ ref('stg_raw__facebook') }}
+)
+
+SELECT * FROM adwords
+UNION ALL
+SELECT * FROM bing
+UNION ALL
+SELECT * FROM criteo
+UNION ALL
+SELECT * FROM facebook
